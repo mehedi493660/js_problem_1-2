5df0a50f39a5f63a72984ed7a3ed5d3106f2eee4
mehedi493660 <144321420+mehedi493660@users.noreply.github.com>
2023-10-26 02:04:48 +0600

    Add files via upload

---
+++ problem/prb.js
@@ -0,0 +1,52 @@
+var money = 12;
+
+if(money >20 && money < 30){
+    console.log("you can buy a singara")
+}
+else if(money >=30 && money <=45){
+    console.log("singara , somocha and bun or each one,")
+}
+else if(money >=60 && money <=105){
+    console.log("singara , somocha and bun or each one either burger,")
+}
+else if(money <20){
+    console.log("take a glass of water and go home")
+}
+else {
+    console.log("if you have more money go to kacchimosai")
+}
+
+
+
+
+// var sub1 = 75;
+// var sub2 = 98;
+// var sub3 = 87;
+// var sub4 = 90;
+// var number_of_sub = 4;
+
+// var total_num = sub1+sub2+sub3+sub4;
+// var avg = total_num/number_of_sub;
+// console.log(avg);
+
+// if(avg >=90){
+//     console.log("s grade")
+// }
+// else if(avg >=80 && avg <=90){
+//     console.log("a grade")
+// }
+// else if(avg >=70 && avg <=80){
+//     console.log("b grade")
+// }
+// else if(avg >=60 && avg <=70){
+//     console.log("c grade")
+// }
+// else if(avg >=50 && avg <=60){
+//     console.log("d grade")
+// }
+// else if(avg >=40 && avg <=50){
+//     console.log("e grade")
+// }
+// else if(avg <=40){
+//     console.log("f grade")
+// }
